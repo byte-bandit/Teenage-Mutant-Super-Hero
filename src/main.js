5903b6802f96c37deb6eedf7450d57771a11225c
Christian Lohr <gitlab@knanet.de>
2016-02-17 22:14:41 +0100

    Ditch pandaJS for phaser

---
+++ src/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var Mutate = window.Mutate || {};
+
+(function() {
+  Mutate.Engine = function(width, height) {
+    this.game = new Phaser.Game(width, height, Phaser.AUTO, "", {preload: this.onPreload, create: this.onCreate, update: this.onUpdate, render: this.onRender}, false, false);
+
+    Mutate.game = this.game;
+  };
+
+  Mutate.Engine.prototype = {
+    constructor: Mutate.Engine,
+
+    destroy: function() {
+      this.game.destroy();
+    },
+
+    onPreload: function() {
+      this.game.scale.pageAlignHorizontally = true;
+      this.game.scale.pageAlignVertically = true;
+      this.game.scale.refresh();
+    },
+
+    onUpdate: function() {
+    },
+
+    onCreate: function() {
+    },
+
+    onRender: function() {
+    }
+  };
+})();
