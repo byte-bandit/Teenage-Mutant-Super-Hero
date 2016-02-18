ecca88f5a8473abc5e1f63fce346ce16dc720171
Christian Lohr <christian.lohr@ganshorn.de>
2016-02-18 17:02:03 +0100

    Draw map with clickable buildings

--- src/map.js
+++ src/map.js
@@ -8,10 +8,73 @@ Mutate.Map.prototype = {
   constructor: Mutate.Map,
 
   create: function() {
+    this.hotspots = [];
 
+  	this.game.add.sprite(0, 0, 'msLayer4');
+
+    this.game.add.sprite(0, 0, 'msLayer3');
+    this.createHotspot(543, 260, 'airport');
+    this.createHotspot(188, 255, 'hospital');
+    this.createHotspot(1147, 294, 'powerplant');
+
+  	this.game.add.sprite(0, 0, 'msLayer2');
+    this.createHotspot(330, 502, 'tracks');
+    this.createHotspot(860, 355, 'library');
+    this.createHotspot(1242, 576, 'zoo');
+
+  	this.game.add.sprite(0, 0, 'msLayer1');
+    this.createHotspot(30, 478, 'home');
+    this.createHotspot(703, 662, 'hole');
+
+
+    this.player = {
+      name: "Morten",
+      life: 100,
+      mutation: 0,
+      iq: 70
+    }
+
+  	var graphics = this.game.add.graphics(0,0);
+  	graphics.beginFill(0xffc400, 0.8);
+  	graphics.drawRoundedRect(16, 16, 256, 128, 8);
+    
+    this.drawText(32, 32, "Name: " + this.player.name);
+    this.drawText(32, 56, "Life: " + this.player.life);
+    this.drawText(32, 80, "Mutation: " + this.player.mutation);
+    this.drawText(32, 104, "IQ: " + this.player.iq);
+
+    this.hotspots.forEach(function(hotspot) {
+      hotspot.inputEnabled = true;
+      hotspot.pixelPerfectOver = true;
+      hotspot.anchor.setTo(0.5, 0.5);
+    });
   },
 
   update: function() {
+    this.hotspots.forEach(function(hotspot) {
+      if (hotspot.input.pointerOver())
+      {
+        hotspot.tint = 0x66FF66;
+      }
+      else
+      {
+        hotspot.tint = 0xFFFFFF;
+      }
+    });
+  },
 
+  createHotspot: function(x, y, name) {
+    this.hotspots.push(this.game.add.sprite(x, y, name));
+  },
+
+  drawText: function(x, y, text) {
+    var text = this.game.add.text(x, y, text);
+    text.font = 'Arial Black';
+    text.fontSize = 16;
+    text.fontWeight = 'bold';
+    text.fill = '#000000';
+    text.stroke = '#ffffff';
+    text.strokeThickness = 2;
+    return text;
   }
 }
--- src/preload.js
+++ src/preload.js
@@ -12,9 +12,23 @@ Mutate.Preload.prototype = {
 
     // Load game assets
     this.load.image('mainMenuBackground', 'media/Background_LD65.png');
+
+    this.load.image('msLayer1', 'media/backgrounds/msLayer1.png');
+    this.load.image('msLayer2', 'media/backgrounds/msLayer2.png');
+    this.load.image('msLayer3', 'media/backgrounds/msLayer3.png');
+    this.load.image('msLayer4', 'media/backgrounds/msLayer4.png');
+
+    this.load.image('airport', 'media/buildings/airport.png');
+    this.load.image('hole', 'media/buildings/hole.png');
+    this.load.image('home', 'media/buildings/home.png');
+    this.load.image('hospital', 'media/buildings/hospital.png');
+    this.load.image('library', 'media/buildings/library.png');
+    this.load.image('powerplant', 'media/buildings/powerplant.png');
+    this.load.image('tracks', 'media/buildings/tracks.png');
+    this.load.image('zoo', 'media/buildings/zoo.png');
   },
 
   create: function() {
-    this.state.start('MainMenu');
+    this.state.start('Map');
   }
 }
